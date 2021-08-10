0b7275a3f3cbe5ae3fc6022c73ed79e599963a16
shashank-k-y <shashank.sky.8@gmail.com>
2021-08-10 14:02:38 +0530

    flake8 fixs

--- movie_mate/watchlist/api/serializers.py
+++ movie_mate/watchlist/api/serializers.py
@@ -1,16 +1,20 @@
-from django.db.models import fields
 from rest_framework import serializers
-from .validators import validate_name
 
 from watchlist.models import Movie
+
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
-    
 
     def validate(self, data):
         if data["name"] == data["description"]:
-            raise serializers.ValidationError("Name and Description can not be same.")
+            raise serializers.ValidationError(
+                "Name and Description can not be same."
+            )
         return data
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too Short.")
