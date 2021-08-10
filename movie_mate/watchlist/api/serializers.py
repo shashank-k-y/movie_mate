8867939c35c9ff0d355c669e381d037702b749ba
shashank-k-y <shashank.sky.8@gmail.com>
2021-08-10 13:57:50 +0530

    use model serializer

--- movie_mate/watchlist/api/serializers.py
+++ movie_mate/watchlist/api/serializers.py
@@ -1,23 +1,14 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .validators import validate_name
 
 from watchlist.models import Movie
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[validate_name])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.name)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+    
 
     def validate(self, data):
         if data["name"] == data["description"]:
