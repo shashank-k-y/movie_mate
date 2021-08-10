fa9f75be44ded989fac4bd4ba8df5a654d099bf7
shashank-k-y <shashank.sky.8@gmail.com>
2021-08-10 17:38:03 +0530

    add new custom field

--- movie_mate/watchlist/api/serializers.py
+++ movie_mate/watchlist/api/serializers.py
@@ -4,6 +4,11 @@ from watchlist.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    name_length = serializers.SerializerMethodField()
+
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def get_name_length(self, object):
+        return len(object.name)
