0d0ed92e364c109c82ec441f47e4562fd5733664
shashank-k-y <shashank.sky.8@gmail.com>
2022-02-06 17:35:07 +0530

    model view set

--- movie_mate/watchlist/api/views.py
+++ movie_mate/watchlist/api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -168,14 +167,29 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ReviewSerializer
 
 
-class StreamPlatformAV(viewsets.ViewSet):
-    def list(self, request):
-        query_set = StreamingPlatform.objects.all()
-        serializer = StreamingPlatformSerializer(query_set, many=True)
-        return Response(serializer.data)
+# class StreamPlatformAV(viewsets.ViewSet):
+#     def list(self, request):
+#         query_set = StreamingPlatform.objects.all()
+#         serializer = StreamingPlatformSerializer(query_set, many=True)
+#         return Response(serializer.data)
 
-    def retrieve(self, request, pk):
-        query_set = StreamingPlatform.objects.all()
-        stream_platform = get_object_or_404(query_set, pk=pk)
-        serializer = StreamingPlatformSerializer(stream_platform)
-        return Response(serializer.data)
+#     def retrieve(self, request, pk):
+#         query_set = StreamingPlatform.objects.all()
+#         stream_platform = get_object_or_404(query_set, pk=pk)
+#         serializer = StreamingPlatformSerializer(stream_platform)
+#         return Response(serializer.data)
+
+#     def create(self, request):
+#         serializer = StreamingPlatformSerializer(data=request.data)
+#         if not serializer.is_valid():
+#             return Response(
+#                  serializer.errors,
+#                  status=status.HTTP_400_BAD_REQUEST
+#             )
+#         serializer.save()
+#         return Response(serializer.data)
+
+
+class StreamPlatformAV(viewsets.ModelViewSet):
+    queryset = StreamingPlatform.objects.all()
+    serializer_class = StreamingPlatformSerializer
