9258c09ee0a94aecc5b0860ba520b1066d94b6a5
shashank-k-y <shashank.sky.8@gmail.com>
2022-02-07 18:29:57 +0530

    add reviewr field

--- movie_mate/watchlist/models.py
+++ movie_mate/watchlist/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -29,6 +30,7 @@ class WatchList(models.Model):
 
 
 class Review(models.Model):
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
     ratings = models.PositiveIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
