b3453ef78975983ab703dd9cd7e90b50ede74cc3
shashank-k-y <shashank.sky.8@gmail.com>
2022-02-23 04:29:08 +0530

    login endpoint

--- movie_mate/movie_mate/urls.py
+++ movie_mate/movie_mate/urls.py
@@ -5,4 +5,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include("watchlist.api.urls")),
+    path('accounts/', include("user_app.api.urls"))
 ]
