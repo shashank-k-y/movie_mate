933ba173a491c95663fe1d652286cbcfbdd2a80e
shashank-k-y <shashank.sky.8@gmail.com>
2022-02-23 00:06:19 +0530

    Token Authentication

--- movie_mate/movie_mate/settings.py
+++ movie_mate/movie_mate/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'watchlist',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
