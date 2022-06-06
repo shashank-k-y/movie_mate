b08cfea1794418f942c04d54fdda62464ed52a39
shashank-k-y <shashank.sky.8@gmail.com>
2022-06-06 15:15:45 +0530

    api-update

--- movie_mate/watchlist/api/serializers.py
+++ movie_mate/watchlist/api/serializers.py
@@ -12,7 +12,7 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class WatchListSerializer(serializers.ModelSerializer):
-    reviews = ReviewSerializer(many=True, read_only=True)
+    platform = serializers.CharField(source='platform.name')
 
     class Meta:
         model = WatchList
